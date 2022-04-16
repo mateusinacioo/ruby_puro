25ec37c48d64a53258f9ede2ae19d8ba75ca7dec
mateusinacioo <mateus.inacio321@gmail.com>
2022-04-16 13:06:10 -0300

    Exercicios de classe

---
+++ missao_aula07/missao_especial/mercado.rb
@@ -0,0 +1,11 @@
+class Mercado 
+    def initialize(nome, preco)
+        @nome = nome
+        @preco = preco
+    end
+
+    def comprar 
+        puts "Você comprou o produto #{@produto} no valor de #{@preco}"
+    end
+
+end
---
+++ missao_aula07/missao_especial/tempCodeRunnerFile.rb
@@ -0,0 +1,1 @@
+to.nome, produto.preco).comprar
